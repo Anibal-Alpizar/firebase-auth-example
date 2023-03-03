0affe2ad0f33bd7b757b0da8e45668638dd7c244
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-02 23:34:19 -0600

    creating auth

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,5 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCIvNzh3oMC2PXWICznUB4kzDi6G73NHig",
   authDomain: "fir-app-tuto-4da51.firebaseapp.com",
@@ -9,5 +10,4 @@ const firebaseConfig = {
   appId: "1:926166054316:web:bddf71a12fe6a27c46f609",
 };
 export const app = initializeApp(firebaseConfig);
-
-console.log(app);
+export const auth = getAuth(app);
--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,12 +1,11 @@
-console.log('signup form loaded')
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth } from "./firebase.js";
 
-const signupForm = document.querySelector('#signup-form');
+const signupForm = document.querySelector("#signup-form");
 
-signupForm.addEventListener('submit', (e) => {
-
-    e.preventDefault();
-    const email = signupForm['signup-email'].value;
-    const passowrd = signupForm['signup-password'].value
-
-    console.log(email, passowrd);
-})
+signupForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = signupForm["signup-email"].value;
+  const passowrd = signupForm["signup-password"].value;
+  await createUserWithEmailAndPassword(auth, email, passowrd);
+});
